5726a0c76a09ad0a051ebe0dbec1f9cbb497c4ec
subhranshu sahu <subhranshusahu05@gmail.com>
2026-08-28 15:24:43 +0530

    running sum question add

---
+++ 001_Vasu_Questions/01_LC 136, 1480, 1929/01_Running_Sum_Of_1D_Array_1480/Soultion.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> runningSum(vector<int>& nums) {
+    for(int i = 1; i < nums.size(); i++){
+        nums[i] = nums[i] + nums[i-1];
+    } 
+    return nums;
+}
+
+int main(){
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    
+    // Create a vector of size n
+    vector<int> nums(n);
+    
+    cout << "Enter " << n << " numbers: ";
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i]; // Taking user input for each element
+    }
+    
+    // Calculate running sum
+    vector<int> result = runningSum(nums);
+    
+    // Print the output
+    cout << "Running Sum: ";
+    for(int x : result) {
+        cout << x << " ";
+    }
+    cout << endl;
+    
+    return 0;    
+}
